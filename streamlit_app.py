cc046f2def655c0ce2afff3f5e867f619e5be3e8
FlexTrade1234 <krishna33.a.s@gmail.com>
2025-05-28 10:21:37 +0200

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -121,6 +121,6 @@ else:
 
 # Add logo to sidebar
 st.sidebar.image("iGRIDS.jpg", width=200)  # Adjust width as needed
-st.sidebar.image("Screenshot 2024-09-30 at 9.26.48 PM.png")
+#st.sidebar.image("Screenshot 2024-09-30 at 9.26.48 PM.png")
 #st.sidebar.write("Please send your queries at flexiblemarket0@gmail.com by your registered email id.")
 pg.run()
